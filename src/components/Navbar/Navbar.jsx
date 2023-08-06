851bc78ff133e54f61019eff9ac7576abb7a644c
tolgazorlu <tolgazorlu17@gmail.com>
2023-08-06 23:29:54 +0300

    Navbar | CV link changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -116,7 +116,7 @@ export default function Navbar() {
             </button>
           </div>
           <a
-          href="https://drive.google.com/file/d/1anNRGXRCtoeil0CPl3_4RaSNNvUd-IUn/view?usp=sharing"
+          href="#"
             className="text-white dark:text-slate-700 bg-primary dark:bg-secondary hover:bg-secondary hover:text-slate-700 dark:hover:bg-primary dark:hover:text-slate-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 "
           >
             Download CV
